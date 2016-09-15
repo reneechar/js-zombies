c86409ea21dd6d65b1f93d76d10dacb7781c2252
Renee <rchar20@gmail.com>
2016-09-14 17:54:20 -1000

    Zombie constructor passing all tests

--- zombies.js
+++ zombies.js
@@ -307,7 +307,13 @@ Player.prototype.equippedWith = function() {
  * @property {number} speed
  * @property {boolean} isAlive      Default value should be `true`.
  */
-
+function Zombie(health, strength, speed) {
+  this.health = health;
+  this.strength = strength;
+  this.speed = speed;
+  this.isAlive = true;
+  var maxHealth = health;
+};
 
 /**
  * Class => FastZombie(health, strength, speed)
